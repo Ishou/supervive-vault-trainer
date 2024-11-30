d0a4aab861143b88cf1508b106c7bc9368f0875d
Ishou <Ishou@users.noreply.github.com>
2024-11-30 02:28:21 +0100

    style: install mui

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,25 @@
+"use client";
+
+import { Button, Card, CardActions, CardContent } from "@mui/material";
+import { Gauge } from "@mui/x-charts";
+
 export default function Home() {
   return (
-    <div>
-      <main>
-        <h1>Supervive Vault Trainer</h1>
-      </main>
-    </div>
+    <>
+      <Card>
+        <CardContent>
+          <Gauge
+            className="mx-auto"
+            width={100}
+            height={100}
+            value={5}
+            text=""
+          />
+        </CardContent>
+        <CardActions>
+          <Button>Go!</Button>
+        </CardActions>
+      </Card>
+    </>
   );
 }
